0b4736316de9b15bcbadba09287f1ae85384466c
Leonardo Alves <leunardosevla@gmail.com>
2018-04-16 11:52:58 -0300

    added todo repository methods

--- projetolp/src/repository/TodoRepository.java
+++ projetolp/src/repository/TodoRepository.java
@@ -1,5 +1,25 @@
 package repository;
 
-public class TodoRepository {
+import model.Todo;
+
+public class TodoRepository extends AbstractDatabase<Todo> {
+
+	public TodoRepository() {
+		super(Todo.class);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void saveTodo (Todo t) {
+		this.saveEntity(t);
+	}
+	
+	public Todo getTodo (long id) {
+		return this.getEntity(id);
+	}
+	
+	public void deleteTodo(Todo t) {
+		this.deleteEntity(t);
+	}
+	
 
 }
